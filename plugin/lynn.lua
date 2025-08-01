927824532c8bb23f709994e57881ea270ccb95e9
robin <comfysage@isabelroses.com>
2025-08-01 19:42:53 +0200

    feat: add `PackUpdate` command

--- plugin/lynn.lua
+++ plugin/lynn.lua
@@ -1,3 +1,14 @@
+vim.api.nvim_create_user_command("PackUpdate", function(props)
+    local names = props.fargs
+    if #names == 0 then
+      vim.api.nvim_echo({ {"no plugin names given"} }, false, { err = true })
+      return
+    end
+    vim.pack.update(props.fargs)
+end, {
+    nargs = "*",
+    complete = "packadd",
+})
 vim.api.nvim_create_user_command("PackSync", function()
     require("lynn").sync()
 end, {})
